0db162a8272c1da432a470d793bc4312ad9a2c3d
Thiagovb08 <ferreira.thiago21@escola.pr.gov.br>
2025-07-30 12:52:39 +0000

    pergunta do java

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criacartao(
+    "futebol",
+    "quantos mundiais tem o palmeiras?",
+    "o palmeiras nao tem mundial"
+)
+criacartao(
+    "futebol",
+    "quantas copas do brasil tem o gremio",
+    "o gremio tem 5"
+)
+
+criacartao(
+    "qual o maior idolo do gremio",
+    "renato gaucho"
+)
+criacartao(
+    "futebol",
+    "maior idolo da seleçao brasileira",
+    "renato gaucho"
+)
+criacartao(
+    "futebol",
+    "maior treinador da historia",
+    "sir alex ferguson"
+)
